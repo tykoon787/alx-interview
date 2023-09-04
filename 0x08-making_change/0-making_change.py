a95de3238d6169a8e9378a2931fec13d5b1a144d
tykoon787 <tykoon787@protonmail.com>
2023-09-04 13:10:00 +0300

    Walk in the park

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Make change on Coins."""
+
+
+def makeChange(coins, total):
+    """ Implementation of the make change."""
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
+    if total == 0:
+        return num_coins
+    else:
+        return -1
